898c29e00cfb1780bd96b5493178bc5e60ce7225
carlitolr96 <carlosluis9631@gmail.com>
2022-06-21 08:32:18 -0400

    One Commit

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
 
 const Formulario = () => {
 	return (
@@ -7,30 +7,56 @@ const Formulario = () => {
 			<Formik
 				initialValues={{
 					nombre: '',
-					correo: ''
+					apellido: '',
+					correo: '',
+					contraseña: ''
 				}}
-				onSubmit={() => {
+				validate={(valores) => {
+					const errores = {};
+
+					if(!valores.nombre){
+						errores.nombre = 'Por favor ingrese un nombre'
+					}
+
+					return errores;
+				}}
+				onSubmit={(valores) => {
 					console.log('Formulario enviado');
 				}}
 			>
-				{( {values, handleSubmit, handleChange, handleBlur} ) => (
+				{( {values, errors, handleSubmit, handleChange, handleBlur} ) => (
 					<form className="formulario" onSubmit={handleSubmit}>
+						{console.log(errors)}
 						<div>
-							<img src="https://edesur.com.do/media/ft2g4dvn/logo.png" alt="Edesur-logo" height="90px" />
+							<img src="https://www.edesur.com.do/media/ft2g4dvn/logo.png" alt="Edesur-logo" height="90px" />
 						</div>
+
 						<div>
 							<label htmlFor="nombre">Nombre</label>
 							<input 
 								type="text" 
 								id="nombre" 
 								name="nombre" 
-								placeholder="John Doe" 
+								placeholder="Ingrese su nombre" 
 								value={values.nombre}
 								onChange={handleChange}
+								onBlur={handleBlur}
 							/>
 						</div>
 						<div>
-							<label htmlFor="correo">Correo</label>
+							<label htmlFor="nombre">Apellido</label>
+							<input 
+								type="text" 
+								id="apellido" 
+								name="apellido" 
+								placeholder="Ingrese su apellido" 
+								value={values.apellido}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+						</div>
+						<div>
+							<label htmlFor="correo">Correo Electronico</label>
 							<input 
 								type="email" 
 								id="correo" 
@@ -41,7 +67,44 @@ const Formulario = () => {
 								// onBlur={handleBlur}
 							/>
 						</div>
-						<button type="submit">Enviar</button>
+						<div>
+							<label htmlFor="password">Contraseña</label>
+							<input
+								type="password" 
+								id="password"
+								name="password"
+								placeholder="contraseña"
+								value={values.contraseña}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+						</div>
+						
+						{/* <div>
+							<label htmlFor="provincia">Provincia</label>
+							<Field name="provincia" as="select" id="select">
+								<option value="San Cristobal">San Cristobal</option>
+								<option value="Bani">Bani</option>
+								<option value="San Juan">San Juan</option>
+								<option value="Santo Domingo">Santo Domingo</option>
+								<option value="Pedernales">Pedernales</option>
+								<option value="Azua">Azua</option>
+							</Field>
+						</div> */}
+						
+						<div>
+							<label className="checkbox">
+              					<Field 
+								  type="checkbox" 
+								  name="checked" 
+								  value="checked"
+								  onBlur={handleBlur}
+								/>Aceptas las condiciones de uso.
+            				</label>
+						</div>
+
+						<button type="submit">Confirmar</button>
+
 					</form>
 				)}
 				
---
+++ src/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+    return (
+        <>
+            <h2>Inicio 🤮</h2>
+        </>
+    )
+}
+
+
+export default Inicio;
